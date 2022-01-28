47248722e688fd5f6f4699d88eae945409312bd8
zzy0759 <1783575350@qq.com>
2022-01-28 15:57:22 +0800

    新闻news

--- src/main/java/com/example/epidemicprevention/module/track/entity/Track.java
+++ src/main/java/com/example/epidemicprevention/module/track/entity/Track.java
@@ -34,10 +34,10 @@ public interface update {
     @ApiModelProperty(value = "轨迹详情")
     private String trackInfo;
 
-    @ApiModelProperty(value = "create_time")
+    @ApiModelProperty(value = "track_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Timestamp createTime;
+    private Timestamp trackTime;
 
     @ApiModelProperty(value = "病人id")
     private String patientId;
--- src/main/java/com/example/epidemicprevention/module/vehicle/entity/Vehicle.java
+++ src/main/java/com/example/epidemicprevention/module/vehicle/entity/Vehicle.java
@@ -34,10 +34,10 @@ public interface update {
     @ApiModelProperty(value = "交通工具信息")
     private String vehicleInfo;
 
-    @ApiModelProperty(value = "create_time")
+    @ApiModelProperty(value = "vehicleTime")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Timestamp createTime;
+    private Timestamp vehicleTime;
 
     @ApiModelProperty(value = "病人id")
     private String patientId;
